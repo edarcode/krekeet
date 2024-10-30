3e2f9f86bc01db7dd99f33ae091e6a7c5287b2ed
Edwin Ortiz <edarcode@gmail.com>
2024-10-30 02:18:31 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 // @ts-check
-import { customRouting } from "@inox-tools/custom-routing";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    customRouting({
-      "/": "./src/pages/index/index.astro",
-    }),
-  ],
-});
+export default defineConfig({});
