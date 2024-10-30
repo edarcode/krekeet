5bfb28caa585e8192eeb83aefae41833b74eddc0
Edwin Ortiz <edarcode@gmail.com>
2024-10-30 11:31:40 -0500

    refactor

---
+++ src/pages/pages.ts
@@ -0,0 +1,8 @@
+import { CLIENT_BASE_URL } from "../consts/urls";
+
+export const PAGES = {
+  index: {
+    href: `${CLIENT_BASE_URL}/`,
+    display: "Inicio",
+  },
+};
--- src/pages/router.ts
+++
@@ -1,8 +0,0 @@
-import { CLIENT_BASE_URL } from "../consts/urls";
-
-export const ROUTER = {
-  index: {
-    href: `${CLIENT_BASE_URL}/`,
-    display: "Inicio",
-  },
-};
