43067b402c6437e3a92daa4bf5b6a2b362e9559e
Edwin Ortiz <edarcode@gmail.com>
2024-11-07 14:59:58 -0500

    silder fundional

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference path="../.astro/types.d.ts" />
+declare module "aos" {
+  interface AOSOptions {
+    duration?: number; // Duración de la animación en milisegundos
+    offset?: number; // Desplazamiento en píxeles antes de activar la animación
+    once?: boolean; // Ejecuta la animación solo una vez
+    easing?: string; // Easing de la animación
+    delay?: number; // Retraso antes de la animación
+  }
+
+  function init(options?: AOSOptions): void;
+
+  export default {
+    init,
+    refreshHard,
+  };
+}
