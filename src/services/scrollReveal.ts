cad244d1c8380b5dcc9b8a375f51f41ed8af218b
Edwin Ortiz <edarcode@gmail.com>
2024-11-08 12:26:02 -0500

    agg footer

---
+++ src/services/scrollReveal.ts
@@ -0,0 +1,46 @@
+import ScrollReveal from "scrollreveal";
+
+ScrollReveal().reveal(".left-ani", {
+  distance: "999px",
+  origin: "left",
+  opacity: 0,
+  viewFactor: 0.3,
+  duration: 500,
+  reset: true, // Hace que la animación se repita cada vez que el elemento entra en el viewport
+});
+
+ScrollReveal().reveal(".right-ani", {
+  distance: "999px",
+  origin: "right",
+  opacity: 0,
+  viewFactor: 0.3,
+  duration: 500,
+  reset: true,
+});
+
+ScrollReveal().reveal(".top-ani", {
+  distance: "999px",
+  origin: "top",
+  opacity: 0,
+  viewFactor: 0.3,
+  duration: 500,
+  reset: true,
+});
+
+ScrollReveal().reveal(".bottom-ani", {
+  distance: "999px",
+  origin: "bottom",
+  opacity: 0,
+  viewFactor: 0.3,
+  duration: 500,
+  reset: true,
+});
+
+ScrollReveal().reveal(".bottom-ani-footer", {
+  distance: "999px",
+  viewFactor: 0.3,
+  origin: "bottom",
+  opacity: 0,
+  duration: 1000,
+  reset: true,
+});
