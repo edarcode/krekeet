5d7931cf17ae2e56dd665e61afe3b015f8bcf87b
Edwin Ortiz <edarcode@gmail.com>
2024-11-09 09:29:19 -0500

    REFACTor

--- src/service/sendMail.ts
+++ src/service/sendMail.ts
@@ -2,11 +2,12 @@ import { Resend } from "resend";
 
 export const sendMail = async (from?: string, msg?: string) => {
   if (!from || !msg) return;
-  const resend = new Resend(import.meta.env.API_KEY_RESEND);
+
+  const resend = new Resend(import.meta.env.PUBLIC_API_KEY_RESEND);
 
   const result = await resend.emails.send({
     from,
-    to: import.meta.env.FROM_EMAIL_RESEND,
+    to: import.meta.env.PUBLIC_FROM_EMAIL_RESEND,
     subject: "KREKEET",
     html: `<p>${msg}</p>`,
   });
