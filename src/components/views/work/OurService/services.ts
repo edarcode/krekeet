7a1e97bc38bf977b26f854c7ec6aa00b8382de41
Edwin Ortiz <edarcode@gmail.com>
2024-11-09 01:02:02 -0500

    agg vista work

---
+++ src/components/views/work/OurService/services.ts
@@ -0,0 +1,57 @@
+import websites from "./assets/websites.avif";
+import promociones_online from "./assets/promociones_online.avif";
+import diseño_corporativa from "./assets/diseño_corporativa.avif";
+import estrategias_de_comunicacion from "./assets/estrategias_de_comunicacion.avif";
+import image from "./assets/image.avif";
+import consultoría_de_proyectos from "./assets/consultoría_de_proyectos.avif";
+import redes_sociales from "./assets/redes_sociales.avif";
+import identidad_digital from "./assets/identidad_digital.avif";
+import landing_pages from "./assets/landing_pages.avif";
+
+export const SERVICES = [
+  {
+    title: "Servicios",
+    subTitle: "Websites corporativos",
+    img: websites,
+  },
+  {
+    title: "Servicios",
+    subTitle: "Promociones Online",
+    img: promociones_online,
+  },
+  {
+    title: "Creamos",
+    subTitle: "Diseño industrial branding",
+    img: diseño_corporativa,
+  },
+  {
+    title: "Impactamos",
+    subTitle: "Públicidad de medios móviles",
+    img: image,
+  },
+  {
+    title: "Medios",
+    subTitle: "Reporte de campañas",
+    img: consultoría_de_proyectos,
+  },
+  {
+    title: "Servicios",
+    subTitle: "Estrategias de comunicación",
+    img: estrategias_de_comunicacion,
+  },
+  {
+    title: "Servicios",
+    subTitle: "Redes sociales",
+    img: redes_sociales,
+  },
+  {
+    title: "Servicios",
+    subTitle: "Identidad digital",
+    img: identidad_digital,
+  },
+  {
+    title: "Servicios",
+    subTitle: "Lading pages",
+    img: landing_pages,
+  },
+];
