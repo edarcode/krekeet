a19741f52d2404db231ecc12de10dba11d290b3a
Edwin Ortiz <edarcode@gmail.com>
2024-11-12 17:58:36 -0500

    agg videos localmente

--- src/components/views/index/Slider/videos.ts
+++ src/components/views/index/Slider/videos.ts
@@ -1,15 +1,20 @@
+import video1 from "./assets/ardilla.mp4";
+import video2 from "./assets/robot.mp4";
+import video3 from "./assets/bloques.mp4";
+import video4 from "./assets/flor.mp4";
+
 export const VIDEOS = [
   {
-    url: "https://res.cloudinary.com/dvmsllkkg/video/upload/v1730864878/krekeet/tk8k29vzigixxwmamcah.mp4",
+    url: video1,
   },
   {
-    url: "https://res.cloudinary.com/dvmsllkkg/video/upload/v1730759652/krekeet/bmuljzcvs7p4z5txjxeo.mp4",
+    url: video2,
   },
   {
-    url: "https://res.cloudinary.com/dvmsllkkg/video/upload/v1730759648/krekeet/bzvmispmrwxvf3jzlyuh.mp4",
+    url: video3,
   },
   {
-    url: "https://res.cloudinary.com/dvmsllkkg/video/upload/v1730759650/krekeet/fh4srybax9hvkhe0i6s3.mp4",
+    url: video4,
   },
   {
     url: "",
