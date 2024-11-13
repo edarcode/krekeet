1ac24e8c6b7aeafc397f3c407ca0a3f4864c10a4
Edwin Ortiz <edarcode@gmail.com>
2024-11-13 14:28:39 -0500

    agg descripción de products

--- src/components/views/products/Products/data.ts
+++ src/components/views/products/Products/data.ts
@@ -23,92 +23,110 @@ import foto18 from "./assets/18.png";
 export const ProductsData: Array<TProduct> = [
   {
     title: "Generación de Leads",
-    description: "Descripción 1",
+    description:
+      "Email Marketing + Landing Page + Publicidad en Redes Sociales Campaña de email marketing con ofertas exclusivas, diseño de una landing page para capturar correos electrónicos, y anuncios en Facebook e Instagram dirigidos a audiencias específicas.",
     url: foto1.src,
   },
   {
     title: "Aumento de Ventas Online",
-    description: "Descripción 2",
+    description:
+      "Desarrollo de E-commerce + SEO + Publicidad Display Creación de una tienda online con carrito de compras, optimización de productos para motores de búsqueda, y anuncios gráficos en sitios web relevantes.",
     url: foto2.src,
   },
   {
     title: "Transformación Digital",
-    description: "Descripción 3",
+    description:
+      "Experiencia del Cliente (Cx) + Email Marketing + Gestión de Reputación Online Encuestas de satisfacción del cliente, newsletters con contenido exclusivo y promociones, y monitoreo de reseñas en línea con respuestas personalizadas.",
     url: foto3.src,
   },
   {
     title: "Estrategia de Comunicación",
-    description: "Descripción 4",
+    description:
+      "Estrategias de Comunicación + Branding y Diseño de Identidad + Evaluación de Marca Definición de la voz y tono de la marca, rediseño del logo y materiales de marca, y encuestas para evaluar la percepción de la marca.",
     url: foto4.src,
   },
   {
     title: "Interacción con el Cliente",
-    description: "Descripción 5",
+    description:
+      "Contenido Interactivo Dinámico + Juegos Interactivos + Banners Interactivos Creación de quizzes y encuestas en el sitio web, desarrollo de juegos simples relacionados con la marca, y banners interactivos en la página principal.",
     url: foto5.src,
   },
   {
     title: "Optimización de Conversación",
-    description: "Descripción 6",
+    description:
+      "Optimización de la Tasa de Conversión (CRO) + Análisis de Datos + Evaluación de UX/UI Pruebas A/B para diferentes versiones de la página, análisis de comportamiento del usuario, y rediseño de la interfaz para mejorar la usabilidad.",
     url: foto6.src,
   },
   {
     title: "Marketing en Redes Sociales",
-    description: "Descripción 7",
+    description:
+      "Marketing en Redes Sociales + Marketing de Influencers + Campañas de Video Publicaciones regulares en redes sociales, colaboración con influencers relevantes, y producción de videos promocionales para YouTube y Facebook.",
     url: foto7.src,
   },
   {
     title: "Publicidad Local",
-    description: "Descripción 8",
+    description:
+      "Publicidad en Medios Tradicionales + Vallas Digitales + Digital Signage Anuncios en radio y televisión local, colocación de vallas publicitarias digitales en áreas estratégicas, y pantallas digitales en puntos de venta.",
     url: foto8.src,
   },
   {
     title: "Innovación Tecnológica",
-    description: "Descripción 9",
+    description:
+      "Realidad Aumentada (AR) + Realidad Virtual (VR) + Búsqueda Visual Avanzada Aplicaciones de AR para probar productos virtualmente, experiencias de VR en eventos, y búsqueda visual en la tienda online para encontrar productos similares.",
     url: foto9.src,
   },
   {
     title: "Estrategia de IA Generativa",
-    description: "Descripción 10",
+    description:
+      "Estrategias de IA Generativa + Análisis de Datos + Automatización de Marketing Uso de IA para generar contenido personalizado, análisis predictivo de datos de clientes, y campañas de marketing automatizadas basadas en comportamiento del usuario.",
     url: foto10.src,
   },
   {
     title: "Producción de contenido",
-    description: "Descripción 11",
+    description:
+      "Creación de Contenido Gráfico + Producción de Video + Diseño de Material POP y Artículos Diseño de infografías y gráficos para redes sociales, producción de videos promocionales y tutoriales, y creación de folletos y artículos promocionales.",
     url: foto11.src,
   },
   {
     title: "Investigación de Mercado",
-    description: "Descripción 12",
+    description:
+      "Investigación de Mercado + Evaluación de Marca + Estrategias de Comunicación Estudios de mercado para identificar tendencias y necesidades, encuestas para evaluar la percepción de la marca, y desarrollo de estrategias de comunicación basadas en los hallazgos.",
     url: foto12.src,
   },
   {
     title: "Campañas Publicitarias",
-    description: "Descripción 13",
+    description:
+      "Creación de Campañas Publicitarias + SEM (Publicidad en Motores de Búsqueda) + Publicidad Display Diseño y ejecución de campañas publicitarias integradas, anuncios pagados en Google, y banners publicitarios en sitios web relevantes.",
     url: foto13.src,
   },
   {
     title: "Inbound Marketing",
-    description: "Descripción 14",
+    description:
+      "Inbound Marketing + Marketing de Contenidos + SEO Creación de contenido valioso y relevante, optimización de ese contenido para motores de búsqueda, y estrategias para atraer y convertir visitantes en clientes.",
     url: foto14.src,
   },
   {
     title: "Eventos y Activaciones de Marca",
-    description: "Descripción 15",
+    description:
+      "Eventos y Activaciones de Marca + Experiencia del Cliente (Cx) + Gestión de Reputación Online Organización de eventos promocionales, encuestas de satisfacción durante y después del evento, y monitoreo de la reputación de la marca en línea.",
     url: foto15.src,
   },
   {
     title: "Planificación Estratégica",
-    description: "Descripción 16",
+    description:
+      "Planificación Estratégica + Consultoría de Transformación Digital + Estrategias de Comunicación Recopilación y análisis de datos de clientes, uso de esos datos para personalizar campañas, y aplicación de IA para optimizar las estrategias de marketing.",
     url: foto16.src,
   },
   {
     title: "Marketing Basado en Datos",
-    description: "Descripción 17",
+    description:
+      "Marketing Basado en Datos + Análisis de Datos + Estrategias de IA Generativa Desarrollo de un plan estratégico a largo plazo, asesoramiento en la implementación de tecnologías digitales, y definición de estrategias de comunicación coherentes.",
     url: foto17.src,
   },
   {
     title: "Lets go viral",
-    description: "Descripción 18",
+    description:
+      "Creacion de videos Creacion de campaña enfocada en empresas, enfocadas en viralidad para grandes audiencias y uso de recursos exclusivos.",
     url: foto18.src,
   },
 ];
